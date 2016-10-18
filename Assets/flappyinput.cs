9778d67d08f9d4f6432f1c0d78e10f062f174ffd
ChiTop\Chirou <chirou@gmx.de>
2016-10-18 18:52:41 +0200

    projekt + restart und collision

---
+++ Assets/flappyinput.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class flappyinput : MonoBehaviour {
+	
+	public float jumpforce = 100f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		detectInput();
+
+
+	}
+	void detectInput(){
+		if (Input.GetKeyDown ("b")) {
+			Debug.Log ("ButtonPressed");
+			Jump ();
+		}
+		if (Input.GetKeyDown ("r")) {
+			Time.timeScale = 1;
+			Application.LoadLevel (Application.loadedLevel);
+		}
+	}
+
+	void Jump(){
+		gameObject.GetComponent<Rigidbody> ().velocity = new Vector3 (0f, 0f, 0f);
+		gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(0f,jumpforce,0f));
+			
+			}
+	void Dead(){
+		Time.timeScale = 0;
+
+	}
+
+	void OnCollisionEnter(){
+		Debug.Log ("wat");
+		Dead ();
+
+	}
+	//instanciate
+	//mathf.random
+			//JetBrains
+}
---
+++ Assets/pillarbehavoir.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pillarbehavoir : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.GetComponent<Rigidbody> ().velocity = new Vector3 (-3f, 0f, 0f);
+	}
+}
