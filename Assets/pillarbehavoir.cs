72c0d5f3790536c2f3a8ca9ff146ff71911a753a
ChiTop\Chirou <chirou@gmx.de>
2016-10-18 20:18:41 +0200

    Sule bewegt sich gleichmßig, setzt zurück

--- Assets/pillarbehavoir.cs
+++ Assets/pillarbehavoir.cs
@@ -9,7 +9,15 @@ public class pillarbehavoir : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
+
 	void Update () {
 		gameObject.GetComponent<Rigidbody> ().velocity = new Vector3 (-3f, 0f, 0f);
+
+		if (gameObject.transform.position.x < -5) {
+
+			transform.position = new Vector3 (5, Random.Range(-2,2), 0);
+			
+			Debug.Log ("yooooo");
+		}
 	}
 }
